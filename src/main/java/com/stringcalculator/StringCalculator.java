3771d23166d409ccceda871321bac97a52059f36
kailash <suthar.kailash7397@gmail.com>
2024-10-05 17:49:43 +0530

    Add handling for single number and empty string in add() method

--- src/main/java/com/stringcalculator/StringCalculator.java
+++ src/main/java/com/stringcalculator/StringCalculator.java
@@ -3,7 +3,10 @@ package com.stringcalculator;
 public class StringCalculator {
 
 	public int add(String numbers) {
-		return 0;
+		if (numbers.isEmpty())
+			return 0;
+
+		return Integer.parseInt(numbers); // Handles single number input
 	}
 
 }
--- src/main/java/com/stringcalculator/StringCalculatorTest.java
+++ src/main/java/com/stringcalculator/StringCalculatorTest.java
@@ -6,13 +6,31 @@ import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
 
-	/*
-	 * first test for empty string
+	/**
+	 * This test checks that when an empty string is passed to the add() 
+	 * method of the StringCalculator class, it correctly returns 0.
+	 * 
+	 * An empty string represents no numbers, so the expected result is 0.
 	 */
 	@Test
 	public void testEmptyString() {
 		StringCalculator stringCalculator = new StringCalculator();
 		assertEquals(0,stringCalculator.add(""));
 	}
+	
+	/**
+	 * This test verifies that when a single number is passed as a string 
+	 * to the add() method of the StringCalculator class, it correctly 
+	 * returns the integer value of that number.
+	 * 
+	 * For example, when the input is "1", the add() method should return 1.
+	 */
+	@Test
+	public void testSingleNumberReturnsTheNumber() {
+	    StringCalculator stringCalculator = new StringCalculator();
+	    assertEquals(1, stringCalculator.add("1"));
+	}
+
+	
 	
 }
